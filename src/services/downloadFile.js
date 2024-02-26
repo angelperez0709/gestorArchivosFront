46c0154048c96927fa2ddeaa94394ef1d865dc6b
angelperez0709 <angelperez07092000@gmai.com>
2024-02-26 18:35:43 +0100

    add header

--- src/services/downloadFile.js
+++ src/services/downloadFile.js
@@ -4,6 +4,15 @@ const baseUrl =
 export default async function downloadFile(idFile, token) {
   const response = await fetch(`${baseUrl}download.php`, {
     method: "POST",
+    Headers:{
+      "Content-Description": "File Transfer",
+      "Content-Disposition": "attachment; filename=download-filename",
+      "Accept": "/",
+      "Accept-Encoding": "gzip, deflate, br,zstd",
+      "Accept-Language": "es-ES,es;q=0.9",
+      "Origin": "https://gestor-archivos-front.vercel.app",
+      "Referer": "https://gestor-archivos-front.vercel.app/",
+    },
     body: JSON.stringify({ idFile,token }),
   }).then((res) => {
     return res.blob().then((blob) => {
