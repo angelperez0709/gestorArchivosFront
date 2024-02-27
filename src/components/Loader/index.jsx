d972b9cce36bc3c8a1ad80ab0730738b578a8259
angelperez0709 <angelperez07092000@gmai.com>
2024-02-27 08:22:26 +0100

    add index to loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,7 @@
 import "./style.css";
 export default function Loader() {
   return (
-    <div className="absolute top-1/4 left-2/4 container-loader">
+    <div className="absolute top-1/4 left-2/4 container-loader z-50">
       <div className="custom-loader"></div>
     </div>
   );
